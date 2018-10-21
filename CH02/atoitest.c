feaa07ba948596dc9906bd7221527c690b1d4d20
Wei-Tsung Su <suwt@au.edu.tw>
2018-10-22 07:20:48 +0800

    add CH02 > atoitest.c

---
+++ CH02/atoitest.c
@@ -0,0 +1,12 @@
+/* CH02 > atoitest.c */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main() {
+
+    char strNum[] = "123";
+    printf("strNum = %s\n", strNum);
+    printf("atoi(strNum) + 2 = %d\n", atoi(strNum) + 2);
+
+    return EXIT_SUCCESS;
+}
