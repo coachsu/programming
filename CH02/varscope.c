1a62c633074489e67c2dc85a084427be324f9aa4
Wei-Tsung Su <ellington.su@gmail.com>
2018-10-24 07:51:40 +0800

    revise varscope.c

--- CH02/varscope.c
+++ CH02/varscope.c
@@ -1,22 +1,33 @@
 /* CH02 > varscope.c */
 #include<stdlib.h>
 #include<stdio.h>
+#include<stdint.h>
 
-int globalVar = 100;
+int power(int, int);
+
+int32_t globalVar = 2;
 
 int main() {
 
-    int localVar = 200;
+    int32_t localVar = 4;
 
-    if(localVar > globalVar) {
-        int blockVar = 300;
-        localVar = blockVar;
-        globalVar = localVar;
+    if(globalVar < 100) {
+        int32_t blockVar;
+        blockVar = power(globalVar, localVar);
+        printf("Global Var. = %d\n", globalVar);
+        printf("Local  Var. = %d\n", localVar);
+        printf("Block  Var. = %d\n", blockVar);
     }
 
-    printf("Global Var. = %d\n", globalVar);
-    printf("Local Var. = %d\n", localVar);
-    //printf("Block num = %d\n", blockNum);
-
     return EXIT_SUCCESS;
+}
+
+int power(int num, int exp) {
+    int result = 1;
+
+    for(int i = 0 ; i < exp ; i = i + 1) {
+        result = result * num;
+    }
+
+    return result;
 }
