dbe8c3976fe297e8accc722faa15554cc6a7c4a8
Wei-Tsung Su <ellington.su@gmail.com>
2019-02-08 20:42:25 +0800

    add function pointer in CH06

---
+++ CH06/withfp.c
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int add(int x, int y){ return x+y;}
+int sub(int x, int y){ return x-y;}
+int mul(int x, int y){ return x*y;}
+
+int (*fun[])(int x, int y) = {add, sub, mul};
+
+int main() {
+  
+  int x, y;
+  int opt;
+  scanf("%d %d", &x, &y);
+  scanf("%d", &opt);
+
+  printf("%d\n", fun[opt](x, y));
+
+  return EXIT_SUCCESS;
+}
